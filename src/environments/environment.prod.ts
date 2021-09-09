c577026a99fa2a8cf46a239c6e374c5174961c5b
David Duarte <david@duartechsolutions.com>
2021-09-09 00:39:08 -0400

    deploy succefull

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000/api/projects',
-  API_AUTH: 'http://localhost:3000',
+  API_URL_FIREBASE: 'https://porfolio-8433c-default-rtdb.firebaseio.com/projects',
+  API_AUTH_FIREBASE_SIGNUP: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+  API_AUTH_FIREBASE_SIGNIN: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+  API_KEY_FIREBASE: 'AIzaSyB1i143xgUQYNvO9BOLHh9N-tLLbYicGDI',
+  API_DEV: 'http://localhost:3000/api/projects/',
+  API_AUTH_DEV: 'http://localhost:3000',
 };
