91bb5c32421cc6e1c48c1f97e896da7e6c0ce14c
Jesus Duarte <jduartedsp@gmail.com>
2023-05-26 11:49:29 -0400

    angular material migration completed

--- src/app/admin/components/projects/projects.component.ts
+++ src/app/admin/components/projects/projects.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Project } from '@core/models/project';
 import { ServiceProjects } from '@core/services/projects/project.service';
