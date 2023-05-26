4b89f908b62fae89f67f96c6d0a5667c05cc7b48
Jesus Duarte <jduartedsp@gmail.com>
2023-05-26 11:15:37 -0400

    v 15 => 16

--- src/app/admin/components/projects/projects.component.ts
+++ src/app/admin/components/projects/projects.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { Router } from '@angular/router';
 import { Project } from '@core/models/project';
 import { ServiceProjects } from '@core/services/projects/project.service';
