6dfe45f9363de62834f848d4a1950ca519083c2e
Jesus Duarte <jduartedsp@gmail.com>
2023-05-26 14:30:19 -0400

    Tailwind implemented and navbar completed

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  isOpen = false;
+  private router = inject(Router);
   constructor() { }
 
   ngOnInit(): void {
+    this.router.events
+    .pipe(
+      filter((event) => event instanceof NavigationEnd)
+    )
+    .subscribe(() => {
+      this.isOpen = false
+    })
+  }
+
+  toggleMenu(){
+    this.isOpen = !this.isOpen;
   }
 
 }
